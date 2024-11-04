bb188ac180aeb1567fc2c86c48e764dcdd69c4ab
Gilang Swandaru <gilangswandaru27@gmail.com>
2024-11-05 07:10:56 +0800

    "Added new methods and dependencies to BookingController and FrontController, including constructor injection of services and views for booking and front pages."

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreBookingRequest;
+use App\Models\SubscribePackage;
+use App\Services\BookingService;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
     //
+    protected $bookingService;
+
+    public function __construct(BookingService $bookingService)
+    {
+        $this->bookingService = $bookingService;
+    }
+
+    public function booking(SubscribePackage $subscribePackage)
+    {
+        $tax = 0.11;
+        $totalTaxAmount = $tax * $subscribePackage->price;
+        $grandTotalAmount = $subscribePackage->price + $totalTaxAmount;
+
+        return view('booking.checkout', compact('subscribePackage', 'totalTaxAmount', 'grandTotalAmount'));
+    }
+
+    public function bookingStore(SubscribePackage $subscribePackage, StoreBookingRequest $request)
+    {
+        $validated = $request->validated();
+
+        try {
+            $this->bookingService->storeBookingInSession($subscribePackage, $validated);
+        } catch (\Exception $e) {
+            return redirect()->back()->withErrors(['error' => 'Failed to store booking. Please try again.']);
+        }
+        return redirect()->route('front.payment');
+    }
 }
--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,13 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
+use App\Models\Gym;
+use App\Services\FrontService;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     //
+
+    protected $frontService;
+
+    public function __construct(FrontService $frontService)
+    {
+        $this->frontService = $frontService;
+    }
+
     public function index()
     {
-        return 'halaman depan otw';
+        $data = $this->frontService->getFrontPageData();
+        // dd($data);
+        return view('front.index', $data);
+    }
+
+    public function pricing()
+    {
+        $data = $this->frontService->getSubscriptionsData();
+        // dd($data);
+        return view('front.pricing', $data);
+    }
+
+    public function details(Gym $gym)
+    {
+        // dd($gym);
+        return view('front.details', compact('gym'));
+    }
+
+    public function city(City $city)
+    {
+        // dd($city);
+        return view('front.city', compact('city'));
     }
 }
