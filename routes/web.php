d8c698e4121a3802c43585098f0e0c4132c3a72d
Gilang Swandaru <gilangswandaru27@gmail.com>
2024-11-10 16:54:48 +0800

    Implement homepage layout for FitCamp with hero section, features, and gym locations.

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [FrontController::class, 'index'])->name('front.index');
 Route::get('/pricing', [FrontController::class, 'pricing'])->name('front.pricing');
 
-Route::get('/browse/{city::slug}', [FrontController::class, 'city'])->name('front.city');
+Route::get('/browse/{city:slug}', [FrontController::class, 'city'])->name('front.city');
 
 Route::get('/details/{gym:slug}', [FrontController::class, 'details'])->name('front.details');
 
 Route::get('/check-booking', [BookingController::class, 'checkBooking'])->name('front.check_booking');
-Route::post('/check-booking/details}', [BookingController::class, 'checkBookingDetails'])->name('front.check_booking_details');
+Route::post('/check-booking/details', [BookingController::class, 'checkBookingDetails'])->name('front.check_booking_details');
 
 Route::get('/booking/payment', [BookingController::class, 'payment'])->name('front.payment');
 Route::post('/booking/payment', [BookingController::class, 'paymentStore'])->name('front.payment_store');
